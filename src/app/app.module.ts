5046959e6d1e5f6bd73ab003534fa0400b60d3e1
TheValik <90153132+TheValik@users.noreply.github.com>
2021-12-18 19:59:17 +0200

    Begin_components

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { TytulkaComponent } from './tytulka/tytulka.component';
+import { NewsComponent } from './news/news.component';
+import { FormaVzaemidiiComponent } from './forma-vzaemidii/forma-vzaemidii.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    TytulkaComponent,
+    NewsComponent,
+    FormaVzaemidiiComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/forma-vzaemidii/forma-vzaemidii.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forma-vzaemidii',
+  templateUrl: './forma-vzaemidii.component.html',
+  styleUrls: ['./forma-vzaemidii.component.sass']
+})
+export class FormaVzaemidiiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
