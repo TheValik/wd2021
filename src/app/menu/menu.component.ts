d705aa3277ef4d4a2310aab905babeadce28a68b
TheValik <90153132+TheValik@users.noreply.github.com>
2021-12-19 13:24:47 +0200

    menu silet gotov

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -9,6 +9,21 @@ export class MenuComponent implements OnInit {
 
   constructor() { }
 
+  memKlas: string=""
+
+    public displaiStan(): void{
+
+      if(this.memKlas==""){
+        this.memKlas="NONE"
+        console.log("string Visibl");
+        }
+      else{
+        this.memKlas=""
+        console.log("string NONE");
+      }
+
+  }
+
   ngOnInit(): void {
   }
 
